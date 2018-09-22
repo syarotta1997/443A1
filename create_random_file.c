fd6bef79368c4ee67f9fd73602003210bccb95cb
syarotta1997 <helenlu1997@gmail.com>
2018-09-22 09:06:44 -0400

    1

---
+++ create_random_file.c
@@ -0,0 +1,25 @@
+#include "library.h"
+
+int main(int argc, char *argv[]) {
+        if (argc != 4) {
+            printf("Usage: %s <filename> <total bytes> <block size>\n", argv[0]);
+            exit(1);
+        }
+        char *end;
+        long block_size = strtol(argv[3], &end, 10);
+        long total_bytes = strtol(argv[2], &end, 10);
+        char *buf = (char *) malloc( block_size * sizeof(char));
+        
+        FILE *fp = fopen(filename, "w");
+        long cur_bytes = 0;
+        while (cur _bytes < total_bytes){
+            random_array(buf, block_size);
+            fwrite(buf, 1, block_size, fp);
+        }
+        fflush(fp);
+        fclose(fp);
+        free(buf);
+        
+        return 0;
+        
+}
