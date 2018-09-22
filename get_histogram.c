36505cfa69322d374b94db765fee2ff7585d166f
syarotta1997 <helenlu1997@gmail.com>
2018-09-22 09:15:11 -0400

    1

---
+++ get_histogram.c
@@ -0,0 +1,10 @@
+#include "library.h"
+
+int main(int argc, char *argv[]) {
+    
+        if (argc != 3) {
+            printf("Usage: %s <filename> <block size>\n", argv[0]);
+            exit(1);
+        }
+
+}
