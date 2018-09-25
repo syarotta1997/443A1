410fedc3efe14c823bb446c3e3557a40803e28b1
syarotta1997 <helenlu1997@gmail.com>
2018-09-25 19:52:59 -0400

    1

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ blocks = [100,500, 1000, 2500,5000, 7500,10000, 25000,50000,75000,100000,250000,
 
 subprocess.call(["make","clean","all"])
 print("NOW TESTING DISK WRITE ......\n")
-for i in range (0,blocks.count):
+for i in range (0, len(blocks)):
     #calling create_random_file with fixed 5MB file size using different
     #bloci sizes defined in list
     subprocess.call(["create_random_file", "file" ,"5000000",str(blocks[i])])
-print ("NOW TESTING DISK READ ......\n")
+print ("\nNOW TESTING DISK READ ......\n")
     
