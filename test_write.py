ffceea4cf2d64d3e9dfcefe94a57b1ffba57e415
syarotta1997 <helenlu1997@gmail.com>
2018-09-25 19:35:40 -0400

    1

--- test_write.py
+++ test_write.py
@@ -7,5 +7,5 @@ subprocess.call(["make","clean","all"])
 for i in range (0,10):
     #calling create_random_file with fixed 5MB file size using different
     #bloci sizes defined in list
-    subprocess.call(["create_random_file", "file"+ (i+1) ,"5000000",""+blocks[i]])
+    subprocess.call(["create_random_file", "file"+ str(i+1) ,"5000000",str(blocks[i])])
     
