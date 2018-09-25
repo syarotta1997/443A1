7042e09d740917722311da62583276adb5d0eec6
syarotta1997 <helenlu1997@gmail.com>
2018-09-25 19:58:52 -0400

    1

--- test.py
+++ test.py
@@ -9,5 +9,11 @@ for i in range (0, len(blocks)):
     #calling create_random_file with fixed 5MB file size using different
     #bloci sizes defined in list
     subprocess.call(["create_random_file", "file"+str(i) ,"5000000",str(blocks[i])])
+    print("\n")
 print ("\nNOW TESTING DISK READ ......\n")
-    
+for i in range (0, len(blocks)):
+    #calling create_random_file with fixed 5MB file size using different
+    #bloci sizes defined in list
+    subprocess.call(["get_histogram", "file"+str(i) , str(blocks[i])])
+    print("\n")
+print("\n END OF TEST")
