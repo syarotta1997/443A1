28a828826b982c6548c91c8fc6f7402aaada96d0
syarotta1997 <helenlu1997@gmail.com>
2018-09-25 20:35:31 -0400

    1

--- library.c
+++ library.c
@@ -3,6 +3,6 @@
 void random_array(char* array, long bytes){
     for (int i = 0; i < (int)bytes ; i++){
         array[i] =  'A' + (rand() % 26);
-        printf("%c, "array[i]);
+        printf("%c, ",array[i]);
     }
 }
